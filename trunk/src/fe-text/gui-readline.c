svn2github/irssi

commit 56aa1081ae018906f85e39d583fb4ae2a5e031af

Fix delete_{next,previous}_word bindings, patch by Emanuele Giaquinta
git-svn-id: http://svn.irssi.org/repos/irssi@4043 dbcabf3a-b0e7-0310-adc4-f8d773084564

trunk/src/fe-text/gui-readline.c
@@ -1105,8 +1105,8 @@ void gui_readline_init(void)
 	key_bind("backspace", "", "backspace", NULL, (SIGNAL_FUNC) key_backspace);
 	key_bind("delete_character", "", "delete", NULL, (SIGNAL_FUNC) key_delete_character);
 	key_bind("delete_character", NULL, "^D", NULL, (SIGNAL_FUNC) key_delete_character);
-	key_bind("delete_next_word", "meta-d", NULL, NULL, (SIGNAL_FUNC) key_delete_next_word);
-	key_bind("delete_previous_word", "meta-backspace", NULL, NULL, (SIGNAL_FUNC) key_delete_previous_word);
+	key_bind("delete_next_word", NULL, "meta-d", NULL, (SIGNAL_FUNC) key_delete_next_word);
+	key_bind("delete_previous_word", NULL, "meta-backspace", NULL, (SIGNAL_FUNC) key_delete_previous_word);
 	key_bind("delete_to_previous_space", "", "^W", NULL, (SIGNAL_FUNC) key_delete_to_previous_space);
 	key_bind("delete_to_next_space", "", "", NULL, (SIGNAL_FUNC) key_delete_to_next_space);
 	key_bind("erase_line", "", "^U", NULL, (SIGNAL_FUNC) key_erase_line);

commit 906cdd3dd77f50add9646d73a2b188bdfe0389ba

Fix warnings caused by r4043
git-svn-id: http://svn.irssi.org/repos/irssi@4046 dbcabf3a-b0e7-0310-adc4-f8d773084564

trunk/src/fe-text/gui-readline.c
@@ -1105,8 +1105,8 @@ void gui_readline_init(void)
 	key_bind("backspace", "", "backspace", NULL, (SIGNAL_FUNC) key_backspace);
 	key_bind("delete_character", "", "delete", NULL, (SIGNAL_FUNC) key_delete_character);
 	key_bind("delete_character", NULL, "^D", NULL, (SIGNAL_FUNC) key_delete_character);
-	key_bind("delete_next_word", NULL, "meta-d", NULL, (SIGNAL_FUNC) key_delete_next_word);
-	key_bind("delete_previous_word", NULL, "meta-backspace", NULL, (SIGNAL_FUNC) key_delete_previous_word);
+	key_bind("delete_next_word", "", "meta-d", NULL, (SIGNAL_FUNC) key_delete_next_word);
+	key_bind("delete_previous_word", "", "meta-backspace", NULL, (SIGNAL_FUNC) key_delete_previous_word);
 	key_bind("delete_to_previous_space", "", "^W", NULL, (SIGNAL_FUNC) key_delete_to_previous_space);
 	key_bind("delete_to_next_space", "", "", NULL, (SIGNAL_FUNC) key_delete_to_next_space);
 	key_bind("erase_line", "", "^U", NULL, (SIGNAL_FUNC) key_erase_line);